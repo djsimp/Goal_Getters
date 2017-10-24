cd8c032565ba07e3bd1fa2fb3590e60d05177e6d
tapsley <tyler.apsley@gmail.com>
2017-10-24 00:03:18 -0600

    Date pickers

--- app/src/main/java/org/truthdefender/goalgetters/goals/CreateGoalActivity.java
+++ app/src/main/java/org/truthdefender/goalgetters/goals/CreateGoalActivity.java
@@ -1,20 +1,30 @@
 package org.truthdefender.goalgetters.goals;
 
+import android.content.Intent;
+import android.icu.text.DateFormat;
 import android.os.Bundle;
 import android.app.FragmentManager;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.NumberPicker;
 
 import org.truthdefender.goalgetters.R;
+
+import java.util.Date;
 
 /**
  * Created by dj on 10/18/17.
  */
 
 public class CreateGoalActivity extends AppCompatActivity {
+    Button startDateButton;
+    Button deadlineButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +35,54 @@ public class CreateGoalActivity extends AppCompatActivity {
         np.setMinValue(0);
         np.setMaxValue(1000000);
         np.setWrapSelectorWheel(false);
+
+        String currentDate = DateFormat.getDateInstance().format(new Date());
+        startDateButton = (Button) findViewById(R.id.start_date_picker);
+        startDateButton.setText(currentDate);
+        //startDateButton.setText("HI");
+
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar_create_goal);
         setSupportActionBar(toolbar);
         if(getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+
+
+
+//        startDateButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                DialogFragment newFragment = new DatePickerFragment();
+//                newFragment.show(getSupportFragmentManager(), "startDatePicker");
+//            }
+//        });
+
     }
+
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        deadlineButton = (Button) findViewById(R.id.deadline_date_picker);
+        deadlineButton.setText("lol");
+    }
+
+
+
+
 
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    public void showStartDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "startDatePicker");
+    }
+
+    public void showDeadlinePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "deadlinePicker");
     }
 }
---
+++ app/src/main/java/org/truthdefender/goalgetters/goals/DatePickerFragment.java
@@ -0,0 +1,43 @@
+package org.truthdefender.goalgetters.goals;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.icu.util.Calendar;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import org.truthdefender.goalgetters.R;
+
+/**
+ * Created by Tyler on 10/23/2017.
+ */
+
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+
+    boolean isStart;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Button deadline = (Button) getActivity().findViewById(R.id.deadline_date_picker);
+
+        deadline.setText("HI");
+    }
+}
